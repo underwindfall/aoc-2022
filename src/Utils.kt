772a38500a76432760d4b3b1a34dc794be54d981
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-12-17 22:44:47 +0800

    Day01

--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,19 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Splits into space-separate parts of input and maps each part.
+ */
+fun <R> List<String>.parts(map: (List<String>) -> R): List<R> = buildList {
+    var cur = ArrayList<String>()
+    for (s in this@parts) {
+        if (s == "") {
+            add(map(cur))
+            cur = ArrayList()
+            continue
+        }
+        cur.add(s)
+    }
+    if (cur.isNotEmpty()) add(map(cur))
+}
